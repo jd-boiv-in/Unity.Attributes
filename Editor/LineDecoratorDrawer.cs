8e561e1526115c480b84c5b5a7b0ddfd79f0c486
Jean-Denis "JD" Boivin <jd@boiv.in>
2025-03-25 14:37:26 -0400

    Support for Odin order line drawing

--- Editor/LineDecoratorDrawer.cs
+++ Editor/LineDecoratorDrawer.cs
@@ -1,8 +1,32 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
+#if ODIN_INSPECTOR
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using Sirenix.OdinInspector.Editor;
+#endif
+
 namespace JD.Attributes
 {
+#if ODIN_INSPECTOR
+    // Odin version so we can have the line appears BEFORE everything
+    [DrawerPriority(2.0, 0.0, 0.0)]
+    public class LineDecoratorDrawer : OdinAttributeDrawer<LineAttribute>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            var rect = EditorGUI.IndentedRect(EditorGUILayout.GetControlRect(false));
+            rect.x += 3;
+            rect.y += 12 - Attribute.Height / 2f;
+            rect.width -= 6;
+            rect.height = Attribute.Height;
+            SirenixEditorGUI.DrawSolidRect(rect, Attribute.Color.GetColor());
+            
+            CallNextDrawer(label);
+        }
+    }
+#else
     [CustomPropertyDrawer(typeof(LineAttribute))]
     public class LineDecoratorDrawer : DecoratorDrawer
     {
@@ -23,4 +47,5 @@ namespace JD.Attributes
             EditorGUI.DrawRect(rect, lineAttr.Color.GetColor());
         }
     }
+#endif
 }
