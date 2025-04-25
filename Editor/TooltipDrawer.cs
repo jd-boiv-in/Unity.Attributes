7e9b98fc6c8bbd0cefa65eb39b7a47cf28d9bbfb
Jean-Denis Boivin <jd@boiv.in>
2025-04-25 13:11:37 -0400

    Tooltip on shader

---
+++ Editor/TooltipDrawer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace JD.Attributes
+{
+    // By Johannes Deml (send@johannesdeml.com)
+    // See: https://discussions.unity.com/t/can-you-add-editor-tooltips-to-the-shader-properties/713679/3
+    public class TooltipDrawer : MaterialPropertyDrawer
+    {
+        private GUIContent _guiContent;
+
+        private readonly MethodInfo _internalMethod;
+        private readonly Type[] _methodArgumentTypes;
+        private readonly object[] _methodArguments;
+
+        public TooltipDrawer(string tooltip)
+        {
+            _guiContent = new GUIContent(string.Empty, tooltip);
+
+            _methodArgumentTypes = new[] { typeof(Rect), typeof(MaterialProperty), typeof(GUIContent) };
+            _methodArguments = new object[3];
+            
+            _internalMethod = typeof(MaterialEditor).GetMethod("DefaultShaderPropertyInternal", BindingFlags.Instance | BindingFlags.NonPublic, null, _methodArgumentTypes, null);
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty prop, String label, MaterialEditor editor)
+        {
+            _guiContent.text = label;
+            if (_internalMethod == null) return;
+
+            _methodArguments[0] = position;
+            _methodArguments[1] = prop;
+            _methodArguments[2] = _guiContent;
+              
+            _internalMethod.Invoke(editor, _methodArguments);
+        }
+    }
+}
